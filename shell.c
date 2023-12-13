f3e894bc9cd6598504619e2bb424186dc93ab0ec
jakobo007 <oumajamesjoshua@gmail.com>
2023-12-13 22:03:25 +0300

    Trail 4

--- shell.c
+++ shell.c
@@ -39,12 +39,14 @@ int main(void) {
         }
 
         if (pid == 0) {
-
-if (execve(input, (char *[]) {input, NULL}, environ) == -1) {
+            
+if (execve(input, (char *const*) (char *[]) {input, NULL}, environ) == -1) {
     char *error_msg = "./shell: No such file or directory\n";
     write(STDERR_FILENO, error_msg, strlen(error_msg));
     exit(EXIT_FAILURE);
 }
+
+        }
 
         } else {
           int status;
