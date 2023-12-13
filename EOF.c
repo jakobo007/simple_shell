02806e0b8d6d01814096c392c479ff694a6fddf0
jakobo007 <oumajamesjoshua@gmail.com>
2023-12-13 23:44:37 +0300

    changes

--- EOF.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-/**
- * end_of_file - Displays a message and exits the shell.
- */
-void end_of_file(void)
-{
-write(STDOUT_FILENO, "Exiting... \n", strlen("Exiting... \n"));
-exit(EXIT_SUCCESS);
-}
