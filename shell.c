8c45b498e8bf7ad13420fe2791410566a061c051
jakobo007 <oumajamesjoshua@gmail.com>
2023-12-14 22:50:26 +0300

    Trail 21

--- shell.c
+++ shell.c
@@ -1,32 +1,55 @@
-#include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <signal.h>
-/**
- * main - The entry point of the simple shell program.
- * Responsible for reading user input, tokenizing, and executing commands.
- * Returns: Always returns 0.
- */
+
+
+void handlle_signal(int signol)
+{
+    if (signol == SIGINT || signol == SIGQUIT)
+    {
+        fprintf(stderr, "\n");
+        exit(EXIT_SUCCESS);
+    }
+}
+
 int main() {
 char *input = NULL;
 size_t size_of_input = 0;
-signal(SIGTERM, end_of_file);
-signal(SIGINT, new_line);
-while(1)
-{
-write(STDOUT_FILENO, "$ ", 2);
-/**fflush(stdout); **/
-if (getline(&input, &size_of_input, stdin) == -1)
-{
-perror("getline");
-exit(EXIT_FAILURE);
-}
-input[strcspn(input, "\n")] = '\0';
-gettoken(input);
+size_t length;
+
+signal(SIGINT, handle_signal):
+signal(SIGQUIT, handle_signal):
+
+while (1) {
+
+ write(STDOUT_FILENO, "$ ", 2);
+        if (getline(&input, &size_of_input, stdin) == -1) {
+                if (feof(stdin)) {
+                        break;
+                } else {
+                        perror("getline");
+                        return (EXIT_FAILURE);
+                }
+        }
+
+        length = strcspn(input, "\n");
+        if (length > 0 && input[length - 1] == '\n') {
+        input[length] = '\0';
+        }
+
+
+        /** CHECK IF CMD EXIST **/
+        if (access(input, X_OK) == -1) {
+            fprintf(stderr, "%s: No such file or directory\n", "./shell");
+            continue; 
+        }
+
+
+
+
 }
 free(input);
 return (0);
